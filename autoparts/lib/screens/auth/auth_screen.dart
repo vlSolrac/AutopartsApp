8161c83b4c4d12b7b09e51cc7d382e00e6ca512a
vlSolrac <Carlosa.velaluna@gmail.com>
2022-12-15 16:14:31 -0600

    .

--- autoparts/lib/screens/auth/auth_screen.dart
+++ autoparts/lib/screens/auth/auth_screen.dart
@@ -1,8 +1,8 @@
 import 'package:autoparts/screens/auth/sign_in.dart';
 import 'package:autoparts/screens/auth/sign_up.dart';
 import 'package:autoparts/themes/themes.dart';
+import 'package:autoparts/widgets/auth/bubble_indicator_painter.dart';
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
 
 class AuthScreen extends StatefulWidget {
 //Parametes.
@@ -22,6 +22,7 @@ class _AuthScreenState extends State<AuthScreen>
 
   @override
   void dispose() {
+    _pageController.dispose();
     super.dispose();
   }
 
@@ -33,9 +34,139 @@ class _AuthScreenState extends State<AuthScreen>
 
   @override
   Widget build(BuildContext context) {
-    //global variables.
-    final size = MediaQuery.of(context).size;
-    // final statusBar = MediaQuery.of(context).viewPadding;
-    return Scaffold();
+    return Scaffold(
+        body: SingleChildScrollView(
+      physics: const ClampingScrollPhysics(),
+      child: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: <Color>[
+                  ThemeApp.loginGradientStart,
+                  ThemeApp.loginGradientEnd
+                ],
+                begin: FractionalOffset(0.0, 0.0),
+                end: FractionalOffset(1.0, 1.0),
+                stops: <double>[0.0, 1.0],
+                tileMode: TileMode.clamp),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: _buildMenuBar(context),
+              ),
+              Expanded(
+                flex: 2,
+                child: PageView(
+                  controller: _pageController,
+                  physics: const ClampingScrollPhysics(),
+                  onPageChanged: (int i) {
+                    FocusScope.of(context).requestFocus(FocusNode());
+                    if (i == 0) {
+                      setState(() {
+                        right = Colors.white;
+                        left = Colors.black;
+                      });
+                    } else if (i == 1) {
+                      setState(() {
+                        right = Colors.black;
+                        left = Colors.white;
+                      });
+                    }
+                  },
+                  children: <Widget>[
+                    ConstrainedBox(
+                      constraints: const BoxConstraints.expand(),
+                      child: const SignIn(),
+                    ),
+                    ConstrainedBox(
+                      constraints: const BoxConstraints.expand(),
+                      child: const SignUp(),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+
+  Widget _buildMenuBar(BuildContext context) {
+    return Container(
+        width: 300.0,
+        height: 50.0,
+        decoration: const BoxDecoration(
+          color: Color(0x552B2B2B),
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              width: 150,
+              height: 70,
+              decoration: BoxDecoration(
+                  color: Colors.amberAccent,
+                  borderRadius: BorderRadius.circular(50)),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      overlayColor:
+                          MaterialStateProperty.all(Colors.transparent),
+                    ),
+                    onPressed: _onSignInButtonPress,
+                    child: Text(
+                      'Existing',
+                      style: TextStyle(
+                          color: left,
+                          fontSize: 16.0,
+                          fontFamily: 'WorkSansSemiBold'),
+                    ),
+                  ),
+                ),
+                //Container(height: 33.0, width: 1.0, color: Colors.white),
+                Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      overlayColor:
+                          MaterialStateProperty.all(Colors.transparent),
+                    ),
+                    onPressed: _onSignUpButtonPress,
+                    child: Text(
+                      'New',
+                      style: TextStyle(
+                          color: right,
+                          fontSize: 16.0,
+                          fontFamily: 'WorkSansSemiBold'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ));
+  }
+
+  void _onSignInButtonPress() {
+    _pageController.animateToPage(0,
+        duration: const Duration(milliseconds: 500), curve: Curves.decelerate);
+  }
+
+  void _onSignUpButtonPress() {
+    _pageController?.animateToPage(1,
+        duration: const Duration(milliseconds: 500), curve: Curves.decelerate);
   }
 }
